915882a4b15eb49f2e663b35d5b5d0f465cd1b98
vagrant <vagrant@fitpregnancy.dev>
2017-09-14 11:43:11 +0000

    new stage

---
+++ modules/custom/week_block/src/Plugin/Block/WeekBlock.php
@@ -0,0 +1,28 @@
+<?php
+  /**
+ * @file
+ * Contains \Drupal\week_block\Plugin\Block\WeekBlock.
+ */
+namespace Drupal\week_block\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\node\NodeInterface;
+  /**
+ * @Block(
+ *   id = "week-block",
+ *   admin_label = @Translation("week_block_header")
+ * )
+ */
+class WeekBlock extends BlockBase {
+  /**
+  * {@inheritdoc}
+  */
+  public function build() {
+    $fields = \Drupal::entityManager()->getFieldDefinitions('node', 'week');
+    $nodes = $fields['field_week']->getSettings()['allowed_values'];
+    return array(
+    '#theme' => 'week_block',
+    '#nodes' => $nodes,
+    );
+  }
+}
+
