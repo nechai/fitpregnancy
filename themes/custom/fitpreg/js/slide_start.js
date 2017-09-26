558e1a1b96845faa39eb2bc93b8513debf4d9d6a
Andrey Proh <andrey.p@rocksolid.com.ua>
2017-09-26 17:33:52 +0300

    weekly-menu, dump called "fitdump"

--- themes/custom/fitpreg/js/slide_start.js
+++ themes/custom/fitpreg/js/slide_start.js
@@ -1,5 +1,5 @@
-(function($) {
-Drupal.behaviors.drupalbookBehavior = {
+(function($, Drupal, drupalSettings) {
+  Drupal.behaviors.drupalbookBehavior = {
     attach: function () {
       $(document).ready(function(){
         $('.block-region-second').slick({
@@ -11,6 +11,31 @@ Drupal.behaviors.drupalbookBehavior = {
           adaptiveHeight: true,
         });
       });
+
+      var timer;
+      var menu = document.getElementsByClassName("menu--weekly-menu");
+      var li = $(menu).children('ul.menu').children();
+      $.each(li, function (){
+        $(this, '.menu--weekly-menu div.views-row').mouseenter(function() {
+          var aaa = this;
+          timer = setTimeout(function() {
+            var ourRequest = new XMLHttpRequest();
+            ourRequest.open("GET", "/ajax/"+aaa.getElementsByTagName('a')[0].innerHTML);
+            ourRequest.onload = function() {
+              var ourData = jQuery.parseHTML(ourRequest.responseText);
+              var data = $('<div />').append(ourData).find('.view-content').html();
+              if(data !== undefined) {
+                aaa.insertAdjacentHTML('beforeend', data);
+              }
+            };
+            ourRequest.send();
+          }, 500);
+        }).mouseleave(function(){
+          clearTimeout(timer);
+          $('.menu--weekly-menu div.views-row, .attachment-after').remove();
+        });
+      });
+
     }
   };
 })(jQuery, Drupal, drupalSettings);
