4667c8d3ff36869cdaedc9e47e928344ac33104f
nechai <nviwork7@gmail.com>
2017-11-30 17:21:24 +0200

    Connect external slick library. Add slider basic on slick. Add slider styles.

--- themes/custom/fitpreg/js/slide_start.js
+++ themes/custom/fitpreg/js/slide_start.js
@@ -1,41 +1,40 @@
-(function($, Drupal, drupalSettings) {
-  Drupal.behaviors.drupalbookBehavior = {
-    attach: function () {
-      $(document).ready(function(){
-        $('.block-region-second').slick({
-          dots: true,
-          infinite: true,
-          speed: 300,
-          fade: true,
-          slidesToShow: 1,
-          adaptiveHeight: true,
-        });
-      });
+(function ($, Drupal, drupalSettings) {
+    Drupal.behaviors.drupalbookBehavior = {
+        attach: function () {
+            $(document).ready(function () {
+                $('.block-region-second .view-slideshow .view-content').slick({
+                    dots: false,
+                    infinite: true,
+                    speed: 300,
+                    slidesToShow: 5,
+                    slidesToScroll: 5
+                });
+            });
 
-      var timer;
-      var menu = document.getElementsByClassName("menu--weekly-menu");
-      var li = $(menu).children('ul.menu').children();
-      $.each(li, function (){
-        $(this, '.menu--weekly-menu div.views-row').mouseenter(function() {
-          var aaa = this;
-          timer = setTimeout(function() {
-            var ourRequest = new XMLHttpRequest();
-            ourRequest.open("GET", "/ajax/"+aaa.getElementsByTagName('a')[0].innerHTML);
-            ourRequest.onload = function() {
-              var ourData = jQuery.parseHTML(ourRequest.responseText);
-              var data = $('<div />').append(ourData).find('.view-content').html();
-              if(data !== undefined) {
-                aaa.insertAdjacentHTML('beforeend', data);
-              }
-            };
-            ourRequest.send();
-          }, 500);
-        }).mouseleave(function(){
-          clearTimeout(timer);
-          $('.menu--weekly-menu div.views-row, .attachment-after').remove();
-        });
-      });
+            var timer;
+            var menu = document.getElementsByClassName("menu--weekly-menu");
+            var li = $(menu).children('ul.menu').children();
+            $.each(li, function () {
+                $(this, '.menu--weekly-menu div.views-row').mouseenter(function () {
+                    var aaa = this;
+                    timer = setTimeout(function () {
+                        var ourRequest = new XMLHttpRequest();
+                        ourRequest.open("GET", "/ajax/" + aaa.getElementsByTagName('a')[0].innerHTML);
+                        ourRequest.onload = function () {
+                            var ourData = jQuery.parseHTML(ourRequest.responseText);
+                            var data = $('<div />').append(ourData).find('.view-content').html();
+                            if (data !== undefined) {
+                                aaa.insertAdjacentHTML('beforeend', data);
+                            }
+                        };
+                        ourRequest.send();
+                    }, 500);
+                }).mouseleave(function () {
+                    clearTimeout(timer);
+                    $('.menu--weekly-menu div.views-row, .attachment-after').remove();
+                });
+            });
 
-    }
-  };
+        }
+    };
 })(jQuery, Drupal, drupalSettings);
