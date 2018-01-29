c07221151b05b16b385180ba134c0bd2c4022487
Vitalii Nechai <nechai.dev@gmail.com>
2018-01-29 16:44:48 +0200

    Made mosaic animation.

--- themes/custom/fitpreg/js/mosaic-init.js
+++ themes/custom/fitpreg/js/mosaic-init.js
@@ -2,13 +2,23 @@
 
     Drupal.behaviors.mosaicBehavior = {
         attach: function (context, settings) {
-            $('.view-content-by-texonomy-term .view-content').once('setMosaic').masonry({
+            $mosaicContainer = $('.view-content-by-texonomy-term .view-content');
+            $mosaicContainer.masonry({
                 itemSelector: '.views-row',
                 // columnWidth: 320,
                 // percentPosition: true,
                 fitWidth: true,
                 gutter: 30
             });
+            // All code below needed for successful loading ajax's items
+            $mosaicContainer.masonry('reloadItems');
+            $mosaicContainer.masonry('layout');
+            var masonryUpdate = function() {
+                setTimeout(function() {
+                    $mosaicContainer.masonry();
+                }, 20);
+            };
+            $(document).ajaxComplete(masonryUpdate);
         }
     };
 
